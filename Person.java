d9b99aede4c09168e2dee5382127353f9579a6fe
DeAdTuBE2004exe <bharatkulkarni.0000@gmail.com>
2025-05-07 11:00:52 +0530

    pushing updated version

--- Person.java
+++ Person.java
@@ -1,3 +1,17 @@
+class Employee extends Person{
+    int salary, emp_id;
+    Employee(int salary, int emp_id, int age, String name) {
+        super(age, name);
+        this.salary = salary;
+        this.emp_id = emp_id;
+    }
+
+    void display(){
+        super.display();
+        System.out.println("Employee ID: " + emp_id + ", Salary: " + salary);
+    }
+}
+
 public class Person {
     int age;
     String name;
@@ -12,8 +26,10 @@ public class Person {
     }
 
     public static void main(String[] args) {
-        Person person1 = new Person(25, "Alice");
+        // Person person1 = new Person(25, "Alice");
+        // person1.display();
 
-        person1.display();
+        Employee employee1 = new Employee(50000, 101, 30, "Bharat");   
+        employee1.display();
     }
 }
